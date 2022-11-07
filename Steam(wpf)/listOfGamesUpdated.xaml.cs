fc8095e8cb4d51e17fb44a1c799af475c7ebf448
am321 <am321@DESKTOP-5NCTJOV>
2022-11-08 02:54:30 +0300

    list of games upgrade

--- Steam(wpf)/listOfGamesUpdated.xaml.cs
+++ Steam(wpf)/listOfGamesUpdated.xaml.cs
@@ -29,63 +29,6 @@ namespace Steam_wpf_
             gamesLV.SelectedValuePath = "idGame";
         }
 
-        private void genreForGameTB_Loaded(object sender, RoutedEventArgs e)
-        {
-            int index = Convert.ToInt32((sender as TextBlock).Uid);
-
-            List<genresForGame> genreList = DBHelper.sE.genresForGame.Where(x => x.idGame == index).ToList();
-
-            string genres = "жанры: ";
-
-            for (int i = 0; i < genreList.Count; i++)
-            {
-                if(i == genreList.Count - 1)
-                    genres += genreList[i].genres.genreName + ", ";
-                else
-                    genres += genreList[i].genres.genreName;
-            }
-
-            (sender as TextBlock).Text = genres;
-        }
-
-        private void languagesForGameTB_Loaded(object sender, RoutedEventArgs e)
-        {
-            int index = Convert.ToInt32((sender as TextBlock).Uid);
-
-            List<languagesForGame> languageList = DBHelper.sE.languagesForGame.Where(x => x.idGame == index).ToList();
-
-            string languages = "языки: ";
-
-            for (int i = 0; i < languageList.Count; i++)
-            {
-                if (i == languageList.Count - 1)
-                    languages += languageList[i].languages.languageName + ", ";
-                else
-                    languages += languageList[i].languages.languageName;
-            }
-
-            (sender as TextBlock).Text = languages;
-        }
-
-        private void tagsForGameTB_Loaded(object sender, RoutedEventArgs e)
-        {
-            int index = Convert.ToInt32((sender as TextBlock).Uid);
-
-            List<tagsForGame> tagList = DBHelper.sE.tagsForGame.Where(x => x.idGame == index).ToList();
-
-            string tags = "метки: ";
-
-            for (int i = 0; i < tagList.Count; i++)
-            {
-                if (i != tagList.Count - 1)
-                    tags += tagList[i].tags.tagName + ", ";
-                else
-                    tags += tagList[i].tags.tagName;
-            }
-
-            (sender as TextBlock).Text = tags;
-        }
-
         private void ratingForGameTB_Loaded(object sender, RoutedEventArgs e)
         {
 
@@ -98,9 +41,36 @@ namespace Steam_wpf_
 
             gameDescriptionTB.Text = gamesList.Find(x => x.idGame == indexOfGame).gameDescription;
 
-            listOfPublishersDG.ItemsSource = DBHelper.sE.publishersForGame.ToList().Where(x => x.idGame == indexOfGame);
+            publishersLV.ItemsSource = DBHelper.sE.publishersForGame.ToList().Where(x => x.idGame == indexOfGame);
 
-            listOfDevelopersDG.ItemsSource = DBHelper.sE.developersForGame.ToList().Where(x => x.idGame == indexOfGame);
+            developersLV.ItemsSource = DBHelper.sE.developersForGame.ToList().Where(x => x.idGame == indexOfGame);
+        }
+
+        private void tagsLV_Loaded(object sender, RoutedEventArgs e)
+        {
+            int index = Convert.ToInt32((sender as ListView).Uid);
+
+            List<tagsForGame> tagsList = DBHelper.sE.tagsForGame.Where(x => x.idGame == index).ToList();
+
+            (sender as ListView).ItemsSource = tagsList;
+        }
+
+        private void languagesLV_Loaded(object sender, RoutedEventArgs e)
+        {
+            int index = Convert.ToInt32((sender as ListView).Uid);
+
+            List<languagesForGame> languagesList = DBHelper.sE.languagesForGame.Where(x => x.idGame == index).ToList();
+
+            (sender as ListView).ItemsSource = languagesList;
+        }
+
+        private void genresLV_Loaded(object sender, RoutedEventArgs e)
+        {
+            int index = Convert.ToInt32((sender as ListView).Uid);
+
+            List<genresForGame> genresList = DBHelper.sE.genresForGame.Where(x => x.idGame == index).ToList();
+
+            (sender as ListView).ItemsSource = genresList;
         }
     }
 }
---
+++ Steam(wpf)/partialClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_wpf_
+{
+    public partial class games
+    {
+        public string gamePriceUpd
+        {
+            get
+            {
+                return gamePrice + " руб.";
+            }
+        }
+    }
+}
